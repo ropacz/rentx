080e79eb8662593723b8ecb78da15088cad04167
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-22 16:49:23 -0300

    refactor: move files implemented of typeorm to the infra layer

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,48 @@
+import { getRepository, Repository } from "typeorm";
+
+import { User } from "@modules/accounts/infra/typeorm/entities/Users";
+import { ICreateUserDTO } from "@modules/dtos/ICreateUserDTO";
+
+import { IUsersRepository } from "../../../repositories/IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    name,
+    email,
+    password,
+    driver_license,
+    id,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+      id,
+      avatar,
+    });
+
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id_user: string): Promise<User> {
+    const user = await this.repository.findOne({ id: id_user });
+
+    return user;
+  }
+}
+
+export { UsersRepository };
