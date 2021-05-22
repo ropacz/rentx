c91b640646d1c8c187e3e62befc3e3f490dc4794
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-22 13:09:34 -0300

    fix: add new imports and catch application errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import "reflect-metadata";
+import express, { Response, Request, NextFunction } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import "./database";
 
 // eslint-disable-next-line import-helpers/order-imports
 import swaggerFile from "../swagger.json";
 
+import "./database";
+
+import "./shared/container";
+
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 
 const app = express();
@@ -14,6 +20,21 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log(`🚀 Server is running!`);
