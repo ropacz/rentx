9248edf2bc342590b44f9c6f6a85030005aac390
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-22 16:53:08 -0300

    refactor: move middlewares to shared infra layer

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensuereAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "1a42d48e824a29f436d528888f83248b"
+    ) as IPayload;
+
+    const userRepository = new UsersRepository();
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User already not exists!", 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
