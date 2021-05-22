97ea995a2bb1271421da66af858b06bf0c80c121
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-22 12:46:07 -0300

    fix: change category repository interface

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../model/Category";
+import { Category } from "../entities/Category";
 
 // DTO => Data Transfer Object
 interface ICreateCategoryDTO {
@@ -8,9 +8,9 @@ interface ICreateCategoryDTO {
 
 // Interface to implements a new category repository
 interface ICategoriesRepository {
-  create({ name, description }: ICreateCategoryDTO): void;
-  list(): Category[];
-  findByName(name: string): Category;
+  create({ name, description }: ICreateCategoryDTO): Promise<void>;
+  list(): Promise<Category[]>;
+  findByName(name: string): Promise<Category>;
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO };
