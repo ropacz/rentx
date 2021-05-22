d6fb73c217db1190a0ea6d0e8f966aedaee050bd
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-22 13:07:59 -0300

    fix: change controller of  categories list

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
 
-  handle(request: Request, response: Response): Response {
-    const all = this.listCategoriesUseCase.execute();
+    const all = await listCategoriesUseCase.execute();
 
-    return response.json(all);
+    return response.status(200).json(all);
   }
 }
 
