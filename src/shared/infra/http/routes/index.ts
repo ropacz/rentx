b2ff829dd2d05e3e19517536e7ad943c19f7c6ea
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-27 23:23:14 -0300

    fix: import new routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { rentalRoutes } from "./rental.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
@@ -9,7 +11,9 @@ const router = Router();
 
 router.use(authenticateRoutes);
 router.use("/users", usersRoutes);
+router.use("/cars", carsRoutes);
 router.use("/categories", categoriesRoutes);
+router.use("/rentals", rentalRoutes);
 router.use("/specifications", specificationRoutes);
 
 export { router };
