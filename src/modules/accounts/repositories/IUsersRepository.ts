fe4279457724463fd688051ad86326b3709abb11
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-27 23:03:17 -0300

    fix: import

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,4 +1,4 @@
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { User } from "../infra/typeorm/entities/Users";
 
 interface IUsersRepository {
