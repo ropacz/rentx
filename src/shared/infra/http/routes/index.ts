7b42319bc0099425d3989d414a8d7f79253279f1
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-27 23:25:01 -0300

    fix: import rentals routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
-import { rentalRoutes } from "./rental.routes";
+import { rentalRoutes } from "./rentals.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
