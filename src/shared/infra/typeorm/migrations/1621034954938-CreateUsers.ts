62c972dc80fb299498697dda8d8bef7c1d84c8c5
Rodrigo Paczkovski <paczweb@gmail.com>
2021-05-27 23:26:43 -0300

    fix: add property is primary in id

--- src/shared/infra/typeorm/migrations/1621034954938-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1621034954938-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1621034954938 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
