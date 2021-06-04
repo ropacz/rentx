875982db438d04ff6bf3cf761597b85fb970e8a1
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:33:23 -0300

    test: create list categories controller tests

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,69 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+
+let connection: Connection;
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license ) 
+				values('${uuid()}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+			`
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  test("should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Test",
+        description: "Same description",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  test("should not be able to create a new category with name exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Test",
+        description: "Same description",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
