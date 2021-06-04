192b79c66fc448fb4b400ff7caeb9575620cf851
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:36:02 -0300

    fix: add new parameters in create method and implements new methods

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -1,6 +1,8 @@
+import { getRepository, Repository } from "typeorm";
+
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
-import { getRepository, Repository } from "typeorm";
+
 import { Rental } from "../entities/Rental";
 
 class RentalsRepository implements IRentalsRepository {
@@ -14,11 +16,17 @@ class RentalsRepository implements IRentalsRepository {
     car_id,
     expected_return_date,
     user_id,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       expected_return_date,
       user_id,
+      id,
+      end_date,
+      total,
     });
 
     await this.repository.save(rental);
@@ -26,14 +34,32 @@ class RentalsRepository implements IRentalsRepository {
     return rental;
   }
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
 
     return openByCar;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id });
+    const openByUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
 
     return openByUser;
+  }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
+    return rental;
+  }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rentals = await this.repository.find({
+      where: { user_id },
+      relations: ["car"],
+    });
+
+    return rentals;
   }
 }
 
