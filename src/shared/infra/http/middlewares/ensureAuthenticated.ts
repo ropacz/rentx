aeaec6f7a392ba54612ab5b0a6ced78da544d619
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:44:52 -0300

    fix: authentication via token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -22,17 +22,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      "1a42d48e824a29f436d528888f83248b"
-    ) as IPayload;
-
-    const userRepository = new UsersRepository();
-    const user = await userRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError("User already not exists!", 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
