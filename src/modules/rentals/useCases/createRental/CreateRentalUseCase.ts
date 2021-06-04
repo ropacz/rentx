74b089bd539cc89e9c484fb3a36f3acb0e245289
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:39:14 -0300

    fix: add new dependencies and small fixes

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
@@ -17,7 +18,9 @@ class CreateRentalUseCase {
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) {}
 
   async execute({
@@ -39,7 +42,8 @@ class CreateRentalUseCase {
       user_id
     );
 
-    const dateNow = this.dateProvider.getDateNow();
+    const dateNow = this.dateProvider.dateNow();
+
     const compare = this.dateProvider.compareInHours(
       dateNow,
       expected_return_date
@@ -50,7 +54,7 @@ class CreateRentalUseCase {
     }
 
     if (rentalOpenToUser) {
-      throw new AppError("There's rental in progress for user!");
+      throw new AppError("There's a rental in progress for user!");
     }
 
     const rental = await this.rentalsRepository.create({
@@ -58,6 +62,8 @@ class CreateRentalUseCase {
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
