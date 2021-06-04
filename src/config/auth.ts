3842c1eac825d11a8a682ab915c2f73b73cf7022
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:15:03 -0300

    feat: token settings and token refresh

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "1a42d48e824a29f436d528888f83248b",
+  expires_in_token: "15m",
+  secret_refresh_token: "igniteSecretToken",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_date: 30,
+};
