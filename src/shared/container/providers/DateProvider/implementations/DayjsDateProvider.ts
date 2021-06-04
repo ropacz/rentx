184ff154946f3d945a340a2fa44519c4caed8771
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:43:12 -0300

    fix: implements new methods to IDateProvider

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -7,18 +7,37 @@ dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number {
-    const end_date_utc = this.convertUTC(end_date);
-    const start_date_utc = this.convertUTC(start_date);
+    const end_date_utc = this.convertToUTC(end_date);
+    const start_date_utc = this.convertToUTC(start_date);
 
     return dayjs(end_date_utc).diff(start_date_utc, "hours");
   }
 
-  private convertUTC(date: Date): string {
+  convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
   }
 
-  getDateNow(): Date {
+  dateNow(): Date {
     return dayjs().toDate();
+  }
+
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUTC(end_date);
+    const start_date_utc = this.convertToUTC(start_date);
+
+    return dayjs(end_date_utc).diff(start_date_utc, "days");
+  }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
+
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
   }
 }
 
