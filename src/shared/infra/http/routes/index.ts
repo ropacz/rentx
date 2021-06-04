2e743909d3d8c85077cfe66aff1b2abdf8e56e48
Rodrigo Paczkovski <paczweb@gmail.com>
2021-06-04 17:46:45 -0300

    fix: add password route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { passwordRoutes } from "./password.routes";
 import { rentalRoutes } from "./rentals.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -13,6 +14,7 @@ router.use(authenticateRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
 router.use("/categories", categoriesRoutes);
+router.use("/password", passwordRoutes);
 router.use("/rentals", rentalRoutes);
 router.use("/specifications", specificationRoutes);
 
